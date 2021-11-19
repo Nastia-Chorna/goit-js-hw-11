a402891d4e8accca66ba51fb465e537e1588f2f0
Nastia-Chorna <82803581+Nastia-Chorna@users.noreply.github.com>
2021-11-20 00:09:50 +0200

    library added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from "axios";
+import SimpleLightbox from 'simplelightbox';
+
+export async function getImg(name, page) {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&safesearch=true&per_page=40&page=${page}&key=24426317-0934d3933860248e2f67d0ecb`;
+    return await axios.get(url);
+}
